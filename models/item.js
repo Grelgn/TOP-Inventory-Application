768e60c3de0ddbd4a5cfcd1e7401d97fca5648c0
Grelgn <grelgn@hotmail.com>
2024-04-06 18:23:45 +0300

    Set up database schemas

---
+++ models/item.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const ItemSchema = new Schema({
+	name: { type: String, required: true, maxLength: 25 },
+	description: { type: String, required: true, maxLength: 200 },
+	category: { type: Schema.Types.ObjectId, ref: "Category", required: true },
+	price: { type: Number, required: true, min: 1 },
+	numberInStock: { type: Number, required: true },
+});
+
+ItemSchema.virtual("url").get(function () {
+	return `/inventory/item/${this._id}`;
+});
+
+module.exports = mongoose.model("Item", ItemSchema);
