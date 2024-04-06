302bc3416a1d2c504862357327ecfad16d204166
Grelgn <grelgn@hotmail.com>
2024-04-06 23:30:24 +0300

    View all categories, and all items

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -1,46 +1,58 @@
 const Category = require("../models/category");
+const Item = require("../models/item");
 const asyncHandler = require("express-async-handler");
 
 exports.index = asyncHandler(async (req, res, next) => {
-  res.send("NOT IMPLEMENTED: Site Home Page");
+	const [numCategories, numItems] = await Promise.all([
+		Category.countDocuments({}).exec(),
+		Item.countDocuments({}).exec(),
+	]);
+
+	res.render("index", {
+		title: "Inventory Home",
+		category_count: numCategories,
+		item_count: numItems,
+	});
 });
 
 // Display list of all Categories.
 exports.category_list = asyncHandler(async (req, res, next) => {
-  res.send("NOT IMPLEMENTED: Category list");
+	const allCategories = await Category.find().sort({ name: 1 }).exec();
+
+	res.render("category_list", { title: "Category List", category_list: allCategories });
 });
 
 // Display detail page for a specific Category.
 exports.category_detail = asyncHandler(async (req, res, next) => {
-  res.send(`NOT IMPLEMENTED: Category detail: ${req.params.id}`);
+	res.send(`NOT IMPLEMENTED: Category detail: ${req.params.id}`);
 });
 
 // Display Category create form on GET.
 exports.category_create_get = asyncHandler(async (req, res, next) => {
-  res.send("NOT IMPLEMENTED: Category create GET");
+	res.send("NOT IMPLEMENTED: Category create GET");
 });
 
 // Handle Category create on POST.
 exports.category_create_post = asyncHandler(async (req, res, next) => {
-  res.send("NOT IMPLEMENTED: Category create POST");
+	res.send("NOT IMPLEMENTED: Category create POST");
 });
 
 // Display Category delete form on GET.
 exports.category_delete_get = asyncHandler(async (req, res, next) => {
-  res.send("NOT IMPLEMENTED: Category delete GET");
+	res.send("NOT IMPLEMENTED: Category delete GET");
 });
 
 // Handle Category delete on POST.
 exports.category_delete_post = asyncHandler(async (req, res, next) => {
-  res.send("NOT IMPLEMENTED: Category delete POST");
+	res.send("NOT IMPLEMENTED: Category delete POST");
 });
 
 // Display Category update form on GET.
 exports.category_update_get = asyncHandler(async (req, res, next) => {
-  res.send("NOT IMPLEMENTED: Category update GET");
+	res.send("NOT IMPLEMENTED: Category update GET");
 });
 
 // Handle Category update on POST.
 exports.category_update_post = asyncHandler(async (req, res, next) => {
-  res.send("NOT IMPLEMENTED: Category update POST");
+	res.send("NOT IMPLEMENTED: Category update POST");
 });
--- controllers/itemController.js
+++ controllers/itemController.js
@@ -3,40 +3,42 @@ const asyncHandler = require("express-async-handler");
 
 // Display list of all Items.
 exports.item_list = asyncHandler(async (req, res, next) => {
-  res.send("NOT IMPLEMENTED: Item list");
+	const allItems = await Item.find().sort({ name: 1 }).exec();
+
+	res.render("item_list", { title: "Item List", item_list: allItems });
 });
 
 // Display detail page for a specific Item.
 exports.item_detail = asyncHandler(async (req, res, next) => {
-  res.send(`NOT IMPLEMENTED: Item detail: ${req.params.id}`);
+	res.send(`NOT IMPLEMENTED: Item detail: ${req.params.id}`);
 });
 
 // Display Item create form on GET.
 exports.item_create_get = asyncHandler(async (req, res, next) => {
-  res.send("NOT IMPLEMENTED: Item create GET");
+	res.send("NOT IMPLEMENTED: Item create GET");
 });
 
 // Handle Item create on POST.
 exports.item_create_post = asyncHandler(async (req, res, next) => {
-  res.send("NOT IMPLEMENTED: Item create POST");
+	res.send("NOT IMPLEMENTED: Item create POST");
 });
 
 // Display Item delete form on GET.
 exports.item_delete_get = asyncHandler(async (req, res, next) => {
-  res.send("NOT IMPLEMENTED: Item delete GET");
+	res.send("NOT IMPLEMENTED: Item delete GET");
 });
 
 // Handle Item delete on POST.
 exports.item_delete_post = asyncHandler(async (req, res, next) => {
-  res.send("NOT IMPLEMENTED: Item delete POST");
+	res.send("NOT IMPLEMENTED: Item delete POST");
 });
 
 // Display Item update form on GET.
 exports.item_update_get = asyncHandler(async (req, res, next) => {
-  res.send("NOT IMPLEMENTED: Item update GET");
+	res.send("NOT IMPLEMENTED: Item update GET");
 });
 
 // Handle Item update on POST.
 exports.item_update_post = asyncHandler(async (req, res, next) => {
-  res.send("NOT IMPLEMENTED: Item update POST");
+	res.send("NOT IMPLEMENTED: Item update POST");
 });
